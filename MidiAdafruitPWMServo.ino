6dfc9185e71f3555ac0bb1560c633c3d189afd85
bborncr <bentley@crcibernetica.com>
2016-03-29 10:55:02 -0600

    First commit

---
+++ MidiAdafruitPWMServo.ino
@@ -0,0 +1,74 @@
+// Shows how to run three Steppers at once with varying speeds
+//
+// Requires the Adafruit_Motorshield v2 library 
+//   https://github.com/adafruit/Adafruit_Motor_Shield_V2_Library
+// And AccelStepper with AFMotor support 
+//   https://github.com/adafruit/AccelStepper
+
+// This tutorial is for Adafruit Motorshield v2 only!
+// Will not work with v1 shields
+
+#include <Wire.h>
+
+#include <Adafruit_PWMServoDriver.h>
+
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+#define SERVOMIN  150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  600 // this is the 'maximum' pulse length count (out of 4096)
+
+#include "MIDIUSB.h"
+
+int MIDICHANNEL = 0xB0; //MIDI Control Change 1 (0xB0)
+
+void setup()
+{  
+  Serial.begin(115200);
+  //while (!Serial) {
+   // ; // wait for serial port to connect. Needed for native USB
+  //}
+  Serial.println("Ready...");
+
+  pwm.begin();
+  
+  pwm.setPWMFreq(50);  // Analog servos run at ~50 Hz updates
+
+}
+
+void loop(){
+midiEventPacket_t rx;
+    rx = MidiUSB.read();
+    if (rx.header != 0 && rx.byte1 == MIDICHANNEL) {
+      
+          Serial.println(rx.byte3);
+          int val = map(rx.byte3,0,127,SERVOMIN,SERVOMAX);
+          pwm.setPWM(rx.byte2, 0, val);
+        } 
+}
+
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+
+void controlChange(byte channel, byte control, byte value) {
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
